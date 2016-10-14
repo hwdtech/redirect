09cdce5735ae0de747d89389e10f4492eb09bbea
AntonyBang <a-spy1994@yandex.ru>
2016-10-14 11:21:19 +0700

    1
    + Небольшая условность насчет IP на некоторое время

--- RedirectApplication/RedirectApplication/Controllers/ValuesController.cs
+++ RedirectApplication/RedirectApplication/Controllers/ValuesController.cs
@@ -1,17 +1,36 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using System.Web;
+using NGeoIP;
+using NGeoIP.Client;
 namespace RedirectApplication.Controllers
 {
     public class ValuesController : ApiController
     {
         // GET api/values
-        public string Get()
+        public HttpResponseMessage Get()
         {
-            return "value";
+            var browser = HttpContext.Current.Request.Browser.Browser.ToString(); //Какой браузер использует пользователь
+            var OS = HttpContext.Current.Request.Browser.Platform.ToString(); ///Какая ОС используется пользователем
+            var MobileOrNot = HttpContext.Current.Request.Browser.IsMobileDevice.ToString(); //true - пользователь сидить с мобильного устройства. иначе - false
+            var userIP = HttpContext.Current.Request.UserHostAddress.ToString(); //IP пользователя. Пока что определяется как ::1 т.к. локалка
+            var language = Request.Headers.AcceptLanguage.ToString().Substring(0, 2); //Наиболее используемый пользователем язык
+            var nGeoRequest = new Request()
+            {
+                Format = Format.Json,
+                IP = "91.144.189.179" //Из-за того, что локалка - придется пока что использовать заранее указанный IP
+            };
+            var nGeoClient = new NGeoClient(nGeoRequest);
+            var rawData = nGeoClient.Execute();
+            var Country = rawData.CountryName.ToString(); //Страна пользователя
+            var Time = DateTime.Now.ToString(); //Время запроса пользователя
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            return resp;
         }
 
         // GET api/values/5
