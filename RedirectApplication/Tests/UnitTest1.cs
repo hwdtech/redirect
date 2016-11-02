0ae2239f3134272d8b87cd155d216d229965d810
AntonyBang <a-spy1994@yandex.ru>
2016-11-02 23:56:57 +0700

    Issue#2095 Tests Still not working

--- RedirectApplication/Tests/UnitTest1.cs
+++ RedirectApplication/Tests/UnitTest1.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Owin;
+using Microsoft.Owin.Testing;
+using RedirectApplication;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Owin.Host.HttpListener;
 
 namespace Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class RedirectTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public static string baseAddress = "http://*:55129/";
+        private static IDisposable _webApp;
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
         {
+            if (typeof(OwinHttpListener) != null)
+            {
+                _webApp = WebApp.Start<Startup>(url: baseAddress);
+            }
         }
+        [AssemblyCleanup]
+        public static void TearDown()
+        {
+            _webApp.Dispose();
+        }
+        //[TestMethod()]
+        //public void GetMethod()
+        //{
+        //    using (var webApp = WebApp.Start<Startup>("http://localhost:55129/"))
+        //    {
+        //        // Execute test against the web API.
+        //        webApp.Dispose();
+        //    }
+        //}
+        //[TestMethod]
+        //public async Task TestMethod()
+        //{
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        var requestUri = new Uri("http://localhost:55129/");
+        //        await httpClient.GetStringAsync(requestUri);
+        //    }
+        //}
+        [TestMethod]
+        public void GetMethod()
+        {
+
+        }
+
     }
 }
